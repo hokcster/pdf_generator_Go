8321cb0ef8912622d88c43937b800f9298ee7f5a
hokcster <bojkon312@gmail.com>
2024-05-04 22:08:39 +0300

    Add images and fonts to a PDF file

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	pdf.ImageOptions("image.jpg", 10, 10, 30, 0, false, gofpdf.ImageOptions{ImageType: "JPG", ReadDpi: true}, 0, "")
+	pdf.AddFont("Roboto", "", "Roboto-Regular.json")
+	pdf.SetFont("Roboto", "", 14)
 }
 
 //pdf.ImageOptions("image.jpg", 10, 10, 30, 0, false, gofpdf.ImageOptions{ImageType: "JPG", ReadDpi: true}, 0, "")
